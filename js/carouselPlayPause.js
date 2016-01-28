8b1210404c194cd6f5c8897afdfcd2685c8ef19b
NBenitus <benoit.goderre@gmail.com>
2016-01-28 15:01:55 -0500

    Added the "Historique" tab

---
+++ js/carouselPlayPause.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('#myCarousel').carousel({
+        interval:2000,
+        pause: "false"
+    });
+    $('#playButton').click(function () {
+        $('#myCarousel').carousel('cycle');
+    });
+    $('#pauseButton').click(function () {
+        $('#myCarousel').carousel('pause');
+    });
+});
+
+
